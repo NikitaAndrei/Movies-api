b721df140be8a3afea493563f875c3c9f10acbd8
AndreiNikita <117647531+NikitaAndrei@users.noreply.github.com>
2022-12-05 13:17:03 +0200

    On a separate page shows films that will be released soon [Delivers #183881683]

--- rating.php
+++ rating.php
@@ -4,18 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
-    <link href="movie.css" rel="stylesheet" type="text/css">
-    <link href="movie.js" rel="script" type="text/css">
+    <link href="css/rating.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <div class = "wrapper">
     <div class = "logo">
         <div class = "container">
             <p>Mov<span>ies</span></p>
-
         </div>
+        <nav class="navMenu">
+            <a href="movie.php">Kodu</a>s
+            <a href="rating.php">Hinnang</a>
+            <a id="actor" href="Actor.php">Actor</a>
+            <a id="nava" href="info.php">Info</a>
+            <div class="dot"></div>
+        </nav>
     </div>
 </div>
-<script src = "movie.js"></script>
+<div class="hinnang">
+    <h2>Hinnang</h2>
+</div>
+<div class="rating">
+    <div>
+        <img src = "img/mpaa-g.gif" alt = "rating poster">
+        <h3>G</h3>
+        <p>–⠀⠀⠀⠀Vanusepiiranguta</p>
+    </div>
+    <div>
+        <img src = "img/mpaa-pg.gif" alt = "rating poster">
+        <h3>PG</h3>
+        <p>–⠀⠀⠀⠀Soovitatav on vanemate kohalolek</p>
+    </div>
+    <div>
+        <img src = "img/mpaa-pg13.gif" alt = "rating poster">
+        <h3>PG-13</h3>
+        <p>–⠀⠀⠀⠀Alla 13-aastastel ei ole soovitatav vaadata</p>
+    </div>
+    <div>
+        <img src = "img/mpaa-r.gif" alt = "rating poster">
+        <h3>R</h3>
+        <p>–⠀⠀⠀⠀Alla 17-aastastel peab olema täiskasvanud saatja</p>
+    </div>
+    <div>
+        <img src = "img/mpaa-nc17.gif" alt = "rating poster">
+        <h3>NC-17</h3>
+        <p>–⠀⠀⠀⠀Alla 18-aastastel ei ole lubatud vaadata</p>
+    </div>
+</div>
 </body>
 </html>
